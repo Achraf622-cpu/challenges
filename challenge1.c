399eee2794d8c3079e8e21af80d3b52608b1e5e9
Achraf622-cpu <hanzazachraf581@gmail.com>
2024-09-03 21:41:55 +0100

    Update challenge1.c

--- challenge1.c
+++ challenge1.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
-int main() {
-    int tableau[] = {1,2,3,4,5};
-    int taille = sizeof(tableau) / sizeof(tableau[0]);
-    printf("Elements du tabeau : \n");
-    for (int i=0; i < taille; i++)
-    {
-        printf("Element %d : %d\n", i + 1, tableau[i]);
 
+int main() {
+    int tableau[5] = {1,2,3,4,5};
+    for(int i=0;i<5;i++){
+        int a=i+1;
+        printf("pour la case %d on a %d \n",a ,tableau[i]);
     }
-return 0;
 }
